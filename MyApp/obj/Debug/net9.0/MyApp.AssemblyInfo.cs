29d155f3236191407789036e4f9aa4c530ec47e3
Umed <majidowumed@gmail.com>
2026-02-26 14:07:14 +0500

    Delete

--- MyApp/obj/Debug/net9.0/MyApp.AssemblyInfo.cs
+++ MyApp/obj/Debug/net9.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+625a0c373ed7b328cae25401a364043108e48b2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e8a686b64a9d64af525632b42289b6ad5fd1e5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
